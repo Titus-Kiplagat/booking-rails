c9b9eedb573ac31435f74d776aa9b5e4ae2a7ade
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-03-25 11:06:58 +0300

    Create room action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,20 +1,34 @@
 class RoomsController < ApplicationController
-	before_action :set_room, only: [:show]
+  before_action :set_room, only: [:show]
 
-	def index
-		@rooms = Room.all
-	end
+  def index
+    @rooms = Room.all
+  end
 
-	def show
-	end
+  def show; end
 
-	def new
-		@room = Room.new
-	end
+  def new
+    @room = Room.new
+  end
 
-	private
+  def create
+    @room = Room.create(room_params)
+    respond_to do |format|
+      if @room.save
+        format.html { redirect_to @room, notice: 'Room was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
 
-	def set_room
-		@room = Room.find(params[:id])
-	end
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:name, :capacity)
+  end
 end
