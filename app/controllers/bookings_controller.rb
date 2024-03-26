4e104c1aea30e8782fa2c48955af3a7890526880
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-03-26 12:09:18 +0300

    Create a new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,9 +11,24 @@ class BookingsController < ApplicationController
     @booking = Booking.new
   end
 
+	def create
+		@booking = Booking.create(booking_params)
+		respond_to do |format|
+			if @booking.save
+				format.html { redirect_to @booking, notice: 'Booking was successfully created.' }
+			else
+				format.html { render :new, status: :unprocessable_entity }
+			end
+		end
+	end
+
   private
 
   def set_booking
     @booking = Booking.find(params[:id])
   end
+
+	def booking_params
+		params.require(:booking).permit(:start, :end, :room_id, :capacity)
+	end
 end
